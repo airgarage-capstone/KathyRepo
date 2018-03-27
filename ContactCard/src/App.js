91789add41ca17581c1c75c9b921a238ef24bea5
Kathy Nguyen <aznpursazn@asu.edu>
2018-03-27 14:56:36 -0700

    Updated API implementation through AirGarage -- Needs to be better styled

--- ContactCard/src/App.js
+++ ContactCard/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './share.png';
 import './App.css';
 import Contact from './ContactCard.js';
 
 class App extends Component {
+
   render() {
     return (
       <div className="card">
